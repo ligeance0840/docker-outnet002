149bc593482f6ea89321f81e902848208a57923e
ligeance0840 <89499106+ligeance0840@users.noreply.github.com>
2022-04-03 17:10:23 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "93a398d3-3d88-492a-bcc0-f9377eb2b501",
                         "alterId": 0
                     }
                 ],
